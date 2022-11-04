710ee6104c224eb8dc4fd1622e93b601b4a92369
Darren King <medr2dev@gmail.com>
2022-11-04 11:00:09 +0000

    Addition of displaying URL check results in the CLI functionality

--- url_checker/cli.py
+++ url_checker/cli.py
@@ -6,7 +6,7 @@ import url_checker
 
 def user_cli_input_args():
     """
-    How to deal with user input CLI arguments and options
+    How to deal with user input CLI arguments and options.
     """
 
     parser = argparse.ArgumentParser(
@@ -19,8 +19,30 @@ def user_cli_input_args():
         metavar="URLs",
         nargs="+",
         type=str,
-        default=[],
+        default=[],  # sets the command-line argument to an empty list by default
         help="Please enter the URL of one or more websites.",
     )
 
-    return parser.parse_args()
+    parser.add_argument(
+        "-f" "--input-file",
+        metavar="FILE",
+        type=str,
+        default="",
+        help="read URLs from a file",
+    )
+
+    return (
+        parser.parse_args()
+    )  # returns a Namespace object containing the parsed arguments
+
+
+def display_check_results(result, url, error=""):
+    """
+    Display the result of the URL connectivity check.
+    """
+
+    print(f'"{url}" is:', end=" ")
+    if result:
+        print("Online!")
+    else:
+        print(f'"Offline??" \n Error: "{error}"')
